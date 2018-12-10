763e6c5c6fae879a83f69517e0eaeccc59af8e05
Emperorhan <iny27@naver.com>
2018-12-10 15:29:37 +0900

    please

--- eos_boot_steps/00_WALLET_IMPORT.sh
+++ eos_boot_steps/00_WALLET_IMPORT.sh
@@ -1,7 +1,8 @@
 WALLET_PASS="$( jq -r '.WALLET_PASSWD' "00_CONFIG.conf" )"
 EOSIO_PRIV="$( jq -r '.EOSIO_PRODUCER_PRIV_KEY' "00_CONFIG.conf" )"
 
-cleos wallet unlock --password $WALLET_PASS
+# ./cleos.sh wallet unlock --password $WALLET_PASS
 
-cleos wallet import --private-key $EOSIO_PRIV
+./cleos.sh wallet import --private-key $EOSIO_PRIV
+./cleos.sh wallet import --private-key $INIT_PRIV_KEY
 
--- eos_boot_steps/04_TOKEN_CREATE_ISSUE.sh
+++ eos_boot_steps/04_TOKEN_CREATE_ISSUE.sh
@@ -1,4 +1,5 @@
-./cleos.sh push action eosio.token create '["eosio", "1000000000.0000 CR", 0, 0, 0]' -p eosio.token
+./cleos.sh push action eosio.token create '["eosio", "10000000000.0000 CR", 0, 0, 0]' -p eosio.token
+sleep 2s
 ./cleos.sh push action eosio.token issue '["eosio",  "1000000000.0000 CR", "init"]' -p eosio
 
 
